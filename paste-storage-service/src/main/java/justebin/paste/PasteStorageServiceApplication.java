71b31b8d77184ad6b4cb8bd9e0f38c6e2d387d3b
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2023-10-24 22:38:32 +0300

    add paste storage service

---
+++ paste-storage-service/src/main/java/justebin/paste/PasteStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package justebin.paste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteStorageServiceApplication.class, args);
+	}
+
+}
---
+++ paste-storage-service/src/test/java/justebin/paste/PasteStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package justebin.paste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
