0d45c2dc03373a5f9ca00c9a565082baf93bbd33
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2023-10-24 21:02:03 +0300

    add key generation service

---
+++ keygen-service/src/main/java/justebin/keygen/KeygenServiceApplication.java
@@ -0,0 +1,13 @@
+package justebin.keygen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeygenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeygenServiceApplication.class, args);
+	}
+
+}
---
+++ keygen-service/src/test/java/justebin/keygen/KeygenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package justebin.keygen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeygenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
