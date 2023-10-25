99e88d49ba0f5af77a4399a20e62608182a0d94a
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2023-10-25 13:13:55 +0300

    add AuthController to auth-service

---
+++ auth-service/src/main/java/justebin/authservice/AuthController.java
@@ -0,0 +1,25 @@
+package justebin.authservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class AuthController {
+
+    public AuthController() {
+
+    }
+
+    @GetMapping
+    public String registerForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String processRegistration() {
+        return "redirect:/login";
+    }
+}
