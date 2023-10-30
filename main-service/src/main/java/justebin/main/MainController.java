bf1a134c7953fc714274bd2864f108f71b822cdb
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2023-10-30 14:24:55 +0300

    add home controller to MainController.java

--- main-service/src/main/java/justebin/main/MainController.java
+++ main-service/src/main/java/justebin/main/MainController.java
@@ -1,4 +1,14 @@
 package justebin.main;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
 }
