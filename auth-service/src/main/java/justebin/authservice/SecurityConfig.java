89df1b982df61f268fba835c5d59b47277755532
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2023-11-07 04:32:33 +0300

    add bean component userDetailsService

--- auth-service/src/main/java/justebin/authservice/SecurityConfig.java
+++ auth-service/src/main/java/justebin/authservice/SecurityConfig.java
@@ -2,8 +2,14 @@ package justebin.authservice;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +17,14 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null) return user;
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
     }
 }
